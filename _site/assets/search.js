1db8c4ff99dad438ee8470c72a874dafec8c58f9
jinsu-l <dla0830@gmail.com>
2020-02-23 00:52:42 +0900

    change blog theme base

--- _site/assets/search.js
+++
@@ -1,1 +0,0 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/test/2018/06/01/header-image.html"},{'title':"Welcome",'url':"/test2/2018/07/01/welcome.html"},{'title':"테스트 포스트",'url':"/test2/2019/07/01/welcome_copy.html"}]};
